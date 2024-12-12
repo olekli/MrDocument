e3be9783b17f3e637a6839c69f12c467484d15b2
Ole Kliemann <mail@olekliemann.de>
2024-12-12 21:31:53 +0100

    Finetune instructions

--- src/chatgpt.rs
+++ src/chatgpt.rs
@@ -70,7 +70,7 @@ fn default_instructions() -> Vec<ChatCompletionMessage> {
         "role": "system",
         "content": "You will be given a scan of a document. \
             It may consist of one or more pages. \
-            You shall provide as output: \n\
+            You shall provide as output in the language of the document: \n\
             (1) A transcription of the contents of the document. \
             If the document is too large to provide a full transcription, \
             you may omit this. \n\
@@ -81,14 +81,15 @@ fn default_instructions() -> Vec<ChatCompletionMessage> {
             The grammatical number of the word used as class should be singular if possible. \n\
             (4) The source of the document. \
             This could be the author, creator, sender or issuer of the document. \n\
+            The source must be usable as part of a filename and must not contain whitespaces or non-ascii characters. \n\
             (5) Between 2 and 4 keywords describing the content of the document. \n\
             (6) A title describing the document. \
             It should be sufficiently specific to differentiate \
             this particular document from other documents of this class and source, \
             but it should not duplicate words that are already found as class or source. \
             The title must be usable as part of a filename and must not contain whitespaces or non-ascii characters. \n\
-            (7) A date to be associated with the document.\n
-            All output shall be in the language of the document.\n"
+            (7) A date to be associated with the document. \
+            Please favor the date when the document was issued over any other dates found.\n"
     })).unwrap()]
 }
 
